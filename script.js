7872f3b3d7f8a2d2b99d9662c948a8f8977d037b
vukivanoic.vuk@gmail.com <vukivanovic.vuk@gmail.com>
2017-12-14 16:22:17 +0100

    T2- Copy second version from codepen

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var displayImages = document.getElementById("display-images");
+var previous = document.getElementById("prev");
+var next = document.getElementById("next");
+
 var images = [];
 var displayImages = document.getElementById("display-images");
 var previous = document.getElementById("prev");
@@ -7,34 +11,34 @@ var Image = function(id, src, alt, height) {
   this.id = id;
   this.src = src;
   this.alt = alt;
-  this.height = "300px"; // all images same height
+  this.height = "200px"; // all images same height
 };
 
 var jsCertificate = new Image(
   1,
   "http://i63.tinypic.com/5uqwet.jpg",
   "Javascript certificate"
-);
+  );
 var htmlCertificate = new Image(
   2,
   "http://i67.tinypic.com/t819xg.jpg",
   "html certificate"
-);
+  );
 var cssCertificate = new Image(
   3,
   "http://i65.tinypic.com/nffklk.jpg",
   "css certificate"
-);
+  );
 var jqueryCertificate = new Image(
   4,
   "http://i66.tinypic.com/2qx9swi.jpg",
   "jquery certificate"
-);
+  );
 var codecademy = new Image(
   5,
   "http://i64.tinypic.com/20p3qk4.jpg",
   "codecademy"
-);
+  );
 
 images.push(
   jsCertificate,
@@ -42,7 +46,7 @@ images.push(
   cssCertificate,
   jqueryCertificate,
   codecademy
-);
+  );
 
 var ImageCounter = 0;
 
@@ -52,32 +56,43 @@ function slideIt(counter, arr) {
     var slideImageAlt = arr[counter].alt;
     var slideImageHeight = arr[counter].height;
     displayImages.innerHTML =
-      "<img src=" +
-      slideImageSrc +
-      " alt=" +
-      slideImageAlt +
-      " style=height:" +
-      slideImageHeight +
-      " />";
+    "<img src=" +
+    slideImageSrc +
+    " alt=" +
+    slideImageAlt +
+    " style=height:" +
+    slideImageHeight +
+    " />";
 
-    counter === arr.length - 1
-      ? (next.style.display = 'none')
-      : (next.style.display = 'block');
-    counter === 0
-      ? (previous.style.display = 'none')
-      : (previous.style.display = 'block');
+    
+    if (counter === arr.length - 1) {
+      next.setAttribute("class", "slider-buttons disabled");
+    } else if (counter === 0) {
+      previous.setAttribute("class", "slider-buttons disabled");
+    } else {
+      next.setAttribute('class', 'slider-buttons')
+      previous.setAttribute('class', 'slider-buttons')
+    }
   }
 
   followUpImage(counter, arr);
 
   next.addEventListener("click", function() {
-    counter++;
-    followUpImage(counter, arr);
+    if (counter === arr.length - 1) {
+      return false;
+    } else {
+      counter++;
+      followUpImage(counter, arr);
+    }
   });
 
   previous.addEventListener("click", function() {
-    counter--;
-    followUpImage(counter, arr);
+    if (counter <= 0) {
+      return false;
+    } else {
+      counter--;
+      followUpImage(counter, arr);
+    }
   });
 }
 
